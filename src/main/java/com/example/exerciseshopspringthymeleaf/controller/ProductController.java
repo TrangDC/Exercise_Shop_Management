cf5f589f9bc0f0408b3f21008da1e5d489845a56
TrangDC <dinhcongtrang99@gmail.com>
2023-12-31 21:58:53 +0700

    done update product

--- src/main/java/com/example/exerciseshopspringthymeleaf/controller/ProductController.java
+++ src/main/java/com/example/exerciseshopspringthymeleaf/controller/ProductController.java
@@ -6,15 +6,15 @@ import com.example.exerciseshopspringthymeleaf.service.ICategoryService;
 import com.example.exerciseshopspringthymeleaf.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/products")
@@ -69,4 +69,30 @@ public class ProductController {
         }
     }
 
+    @GetMapping("/update/{id}")
+    public ModelAndView updateForm(@PathVariable Long id) {
+        Optional<Product> product = iProductService.findById(id);
+        if (product.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/products/update");
+            modelAndView.addObject("product", product.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/error-404");
+        }
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@Validated @ModelAttribute Product product,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes,
+                         Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("product", product);
+            return "/products/update";
+        } else {
+            iProductService.save(product);
+            redirectAttributes.addFlashAttribute("message", "Update Successfully");
+            return "redirect:/api/products";
+        }
+    }
 }
